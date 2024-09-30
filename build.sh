a2222a547fe4cf33a4485ef3f29bbfd185a01c22
digital-qr <retired64@proton.me>
2024-09-29 23:40:46 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@ python -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-rm -rf public
+rm -rf docs
 reflex init
 reflex export --frontend-only
-unzip frontend.zip -d public
+unzip frontend.zip -d docs
 rm -f frontend.zip
 deactivate
